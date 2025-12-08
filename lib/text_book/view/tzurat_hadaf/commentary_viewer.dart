5fc2a2a5d7c0ca24c8d3d9bb95069ccab9c40675
mosh-dvd <mn0533195305@gmail.com>
2025-12-08 18:01:16 +0200

    Fix tzurat hadaf: use Links for commentaries, add justify text, update Android Gradle

--- lib/text_book/view/tzurat_hadaf/commentary_viewer.dart
+++ lib/text_book/view/tzurat_hadaf/commentary_viewer.dart
@@ -4,9 +4,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:otzaria/models/links.dart';
 import 'package:otzaria/text_book/bloc/text_book_bloc.dart';
 import 'package:otzaria/text_book/bloc/text_book_state.dart';
-import 'package:otzaria/text_book/text_book_repository.dart';
-import 'package:otzaria/models/books.dart';
 import 'package:otzaria/utils/text_manipulation.dart' as utils;
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
+import 'package:otzaria/settings/settings_bloc.dart';
+import 'package:otzaria/settings/settings_state.dart';
 
 class CommentaryViewer extends StatefulWidget {
   final String? commentatorName;
@@ -25,97 +27,64 @@ class CommentaryViewer extends StatefulWidget {
 }
 
 class _CommentaryViewerState extends State<CommentaryViewer> {
-  String? _fullContent;
-  final Map<int, int> _indexToLineMap = {};
-  final ScrollController _scrollController = ScrollController();
-  final Map<int, GlobalKey> _lineKeys = {};
+  final ItemScrollController _scrollController = ItemScrollController();
+  List<Link> _relevantLinks = [];
 
   @override
   void initState() {
     super.initState();
-    _loadFullCommentary();
+    _loadLinks();
   }
 
   @override
   void didUpdateWidget(CommentaryViewer oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (oldWidget.commentatorName != widget.commentatorName) {
-      _loadFullCommentary();
+      _loadLinks();
     } else if (oldWidget.selectedIndex != widget.selectedIndex) {
       _scrollToSelected();
     }
   }
 
-  @override
-  void dispose() {
-    _scrollController.dispose();
-    super.dispose();
-  }
-
-  Future<void> _loadFullCommentary() async {
+  void _loadLinks() {
     if (widget.commentatorName == null) {
       setState(() {
-        _fullContent = null;
-        _indexToLineMap.clear();
+        _relevantLinks = [];
       });
       return;
     }
 
-    final repo = context.read<TextBookBloc>().repository;
-    
-    // Build map of main text index to commentary line
-    _indexToLineMap.clear();
-    print('Building index map for commentator: ${widget.commentatorName}');
-    print('Total links: ${widget.textBookState.links.length}');
-    
-    for (final link in widget.textBookState.links) {
-      final linkTitle = utils.getTitleFromPath(link.path2);
-      if (linkTitle == widget.commentatorName) {
-        _indexToLineMap[link.index1 - 1] = link.index2 - 1;
-        print('Mapped main index ${link.index1 - 1} -> commentary line ${link.index2 - 1}');
-      }
-    }
-    
-    print('Index map size: ${_indexToLineMap.length}');
+    // Get all links for this commentator
+    final links = widget.textBookState.links.where((link) {
+      return utils.getTitleFromPath(link.path2) == widget.commentatorName;
+    }).toList();
 
-    try {
-      final book = TextBook(title: widget.commentatorName!);
-      final content = await repo.getBookContent(book);
-      if (mounted) {
-        setState(() {
-          _fullContent = content;
-        });
-        _scrollToSelected();
-      }
-    } catch (e) {
-      print('Error loading commentary: $e');
-      if (mounted) {
-        setState(() {
-          _fullContent = '';
-        });
-      }
-    }
+    // Sort by index1 to maintain order
+    links.sort((a, b) => a.index1.compareTo(b.index1));
+
+    setState(() {
+      _relevantLinks = links;
+    });
+    
+    _scrollToSelected();
   }
 
   void _scrollToSelected() {
-    print('Commentary [${widget.commentatorName}]: selectedIndex=${widget.selectedIndex}, map size=${_indexToLineMap.length}');
-    
-    if (widget.selectedIndex == null || !_indexToLineMap.containsKey(widget.selectedIndex)) {
-      print('Commentary [${widget.commentatorName}]: No scroll - selectedIndex: ${widget.selectedIndex}, has mapping: ${_indexToLineMap.containsKey(widget.selectedIndex ?? -1)}');
-      return;
-    }
+    if (widget.selectedIndex == null || _relevantLinks.isEmpty) return;
 
-    final lineIndex = _indexToLineMap[widget.selectedIndex];
-    print('Commentary [${widget.commentatorName}]: Scrolling to line $lineIndex for main text index ${widget.selectedIndex}');
-    
-    if (lineIndex != null && _lineKeys.containsKey(lineIndex)) {
+    // Find first link that matches the selected index
+    final targetIndex = _relevantLinks.indexWhere(
+      (link) => link.index1 - 1 == widget.selectedIndex,
+    );
+
+    if (targetIndex != -1 && _scrollController.isAttached) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-        final key = _lineKeys[lineIndex];
-        if (key?.currentContext != null) {
-          Scrollable.ensureVisible(
-            key!.currentContext!,
+        if (_scrollController.isAttached) {
+          _scrollController.scrollTo(
+            index: targetIndex,
             duration: const Duration(milliseconds: 300),
             curve: Curves.easeInOut,
+            alignment: 0.2,
           );
         }
       });
@@ -124,51 +93,67 @@ class _CommentaryViewerState extends State<CommentaryViewer> {
 
   @override
   Widget build(BuildContext context) {
-    if (_fullContent == null) {
-      return Center(
+    if (widget.commentatorName == null) {
+      return const Center(
         child: Text(
-          widget.commentatorName ?? 'בחר מפרש',
-          style: const TextStyle(color: Colors.grey),
+          'בחר מפרש',
+          style: TextStyle(color: Colors.grey),
         ),
       );
     }
 
-    if (_fullContent!.isEmpty) {
-      return const Center(child: Text('התוכן לא נמצא'));
+    if (_relevantLinks.isEmpty) {
+      return const Center(child: Text('אין מפרשים להצגה'));
     }
 
-    final lines = _fullContent!.split('\n');
-    final selectedLineIndex = widget.selectedIndex != null 
-        ? _indexToLineMap[widget.selectedIndex]
-        : null;
-
-    return ListView.builder(
-      controller: _scrollController,
+    return ScrollablePositionedList.builder(
+      itemScrollController: _scrollController,
       padding: const EdgeInsets.all(8.0),
-      itemCount: lines.length,
+      itemCount: _relevantLinks.length,
       itemBuilder: (context, index) {
-        _lineKeys[index] = GlobalKey();
-        final isSelected = index == selectedLineIndex;
+        final link = _relevantLinks[index];
+        final isSelected = widget.selectedIndex != null && 
+                          link.index1 - 1 == widget.selectedIndex;
         
-        return Container(
-          key: _lineKeys[index],
-          color: isSelected ? Colors.yellow.withAlpha(100) : null,
-          padding: const EdgeInsets.symmetric(vertical: 2.0),
-          child: Text(
-            _stripHtmlTags(lines[index]),
-            style: TextStyle(
-              fontSize: widget.textBookState.fontSize * 0.8,
-              fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-            ),
-            textDirection: TextDirection.rtl,
-            textAlign: TextAlign.justify,
-          ),
+        return FutureBuilder<String>(
+          future: link.content,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const SizedBox(height: 50, child: Center(child: CircularProgressIndicator()));
+            }
+
+            return BlocBuilder<SettingsBloc, SettingsState>(
+              builder: (context, settingsState) {
+                String displayText = snapshot.data!;
+                if (settingsState.replaceHolyNames) {
+                  displayText = utils.replaceHolyNames(displayText);
+                }
+                if (widget.textBookState.removeNikud) {
+                  displayText = utils.removeVolwels(displayText);
+                }
+                
+                return Container(
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 2.0),
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: isSelected ? BoxDecoration(
+                    color: Colors.yellow.withOpacity(0.4),
+                    borderRadius: BorderRadius.circular(4),
+                  ) : null,
+                  child: HtmlWidget(
+                    '<div style="text-align: justify; direction: rtl;">$displayText</div>',
+                    textStyle: TextStyle(
+                      fontSize: widget.textBookState.fontSize * 0.8,
+                      fontFamily: settingsState.commentatorsFontFamily,
+                      fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                    ),
+                  ),
+                );
+              },
+            );
+          },
         );
       },
     );
-  }
-
-  String _stripHtmlTags(String html) {
-    return html.replaceAll(RegExp(r'<[^>]*>'), '');
   }
 }
